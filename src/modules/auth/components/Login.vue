2484b1e1a587d7668618eb41ed693914717cda90
Jose Angarita <jose.angarita@inchcape.com>
2025-06-25 23:55:30 -0500

    utility classes for fonts

--- src/modules/auth/components/Login.vue
+++ src/modules/auth/components/Login.vue
@@ -4,7 +4,7 @@ import Card from '@/components/ui/Card.vue';
 </script>
 
 <template>
-  <Card class="p-0 bg-transparent md:bg-white md:p-10">
+  <Card class="p-0 bg-transparent md:bg-white md:p-10 text-heading-m">
     Login
   </Card>
 </template>  
